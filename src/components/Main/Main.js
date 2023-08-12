00690ca101fc77d0dbfe21fd194659dc363e74b5
losbojos <losbojos@yandex.ru>
2023-08-12 19:50:33 +0700

    Верстка и компонент Promo

---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './main.css';
+import Promo from '../Promo/Promo';
+
+function Main(props) {
+
+    return (
+        <main className="main">
+            <Promo />
+        </main >
+    );
+}
+
+export default Main;
---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './promo.css';
+import promoImage from '../../images/promo.svg';
+
+function Promo(props) {
+
+    return (
+        <div className="promo">
+            <img className="promo__banner" alt="баннер" src={promoImage} />
+            <h1 className="promo__header">Учебный проект студента факультета Веб-разработки.</h1>
+            <p className="promo__text">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+        </div>
+    );
+}
+
+export default Promo;
