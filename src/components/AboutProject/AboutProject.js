0d9f2de03106fe7f794c38ec332116f6accacd11
losbojos <losbojos@yandex.ru>
2023-08-13 16:16:52 +0700

    Компонент AboutProject

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './about-project.css';
+
+function AboutProject(props) {
+
+    return (
+        <div className="about-project">
+            <h2 className="about-project__header">О проекте</h2>
+            <hr className="about-project__line"></hr>
+            <ul className="about-project__list">
+                <li className="about-project__item">
+                    <article>
+                        <h3 className="about-project__header">Дипломный проект включал 5 этапов</h3>
+                        <p className="about-project__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                    </article>
+
+                </li>
+                <li className="about-project__item">
+                    <article>
+                        <h3 className="about-project__header">На выполнение диплома ушло 5 недель</h3>
+                        <p className="about-project__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                        <div className="about-project__roadmap">
+                            <ul className="about-project__roadmap-list">
+                                <li className="about-project__roadmap-list-item" style={{ "flexGrow": "1" }} >
+                                    <p className="about-project__roadmap-content about-project__roadmap-content_diagram about-project__roadmap-content_diagram_highlighted">1 неделя</p>
+                                    <p className="about-project__roadmap-content about-project__roadmap-content_text">Back-end</p>
+                                </li>
+                                <li className="about-project__roadmap-list-item" style={{ "flexGrow": "4" }} >
+                                    <p className="about-project__roadmap-content about-project__roadmap-content_diagram">4 недели</p>
+                                    <p className="about-project__roadmap-content about-project__roadmap-content_text">Front-end</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </article>
+                </li>
+            </ul>
+        </div >
+    );
+}
+
+export default AboutProject;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './main.css';
 import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject';
 
 function Main(props) {
 
     return (
         <main className="main">
             <Promo />
+            <AboutProject />
         </main >
     );
 }
