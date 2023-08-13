2e1c60a0ab44375af653f507201894ed85fe91d0
losbojos <losbojos@yandex.ru>
2023-08-13 08:31:13 +0700

    Добавить кнопку Узнать Больше к компоненту Promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -9,6 +9,7 @@ function Promo(props) {
             <img className="promo__banner" alt="баннер" src={promoImage} />
             <h1 className="promo__header">Учебный проект студента факультета Веб-разработки.</h1>
             <p className="promo__text">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+            <button className="promo__button" type="button">Узнать больше</button>
         </div>
     );
 }
