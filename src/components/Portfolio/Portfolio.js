cdca9817ba06e0600b2afb24ebbafd0a94c5e728
losbojos <losbojos@yandex.ru>
2023-08-23 22:07:17 +0700

    rename NavTab to PortfolioRef

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navtab from '../Navtab/Navtab'
+import PortfolioRef from '../PortfolioRef/PortfolioRef'
 
 import './portfolio.css';
 import './portfolio__header.css';
@@ -14,19 +14,19 @@ function Portfolio(props) {
             <h2 className="portfolio__header">Портфолио</h2>
             <ul className="portfolio__list">
                 <li className="portfolio__listitem">
-                    <Navtab
+                    <PortfolioRef
                         title='Статичный сайт'
                         link='https://losbojos.github.io/russian-travel/index.html'
                         showline={false} />
                 </li>
                 <li className="portfolio__listitem">
-                    <Navtab
+                    <PortfolioRef
                         title='Адаптивный сайт'
                         link='https://losbojos.github.io/russian-travel/index.html'
                     />
                 </li>
                 <li className="portfolio__listitem">
-                    <Navtab
+                    <PortfolioRef
                         title='Одностраничное приложение'
                         link='https://losbojos.mesto.nomoreparties.co' />
                 </li>
