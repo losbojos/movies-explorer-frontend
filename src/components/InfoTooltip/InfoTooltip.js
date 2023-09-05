0707a5f172db1f7cccbd0e42217408069af72459
losbojos <losbojos@yandex.ru>
2023-09-05 22:17:11 +0700

    Компоненты Popup и InfoTooltip. Заготовка окна вывода ошибок.

---
+++ src/components/InfoTooltip/InfoTooltip.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Popup from '../Popup/Popup'
+
+import './info-tooltip.css';
+import './info-tooltip__icon.css';
+import './info-tooltip__message.css';
+
+function InfoTooltip(props) {
+
+    const { message, iconLink, isOpen, onClose, afterClose, title } = props;
+
+    const combFunc = () => {
+        onClose();
+        if (afterClose) {
+            afterClose();
+        }
+    }
+
+    console.log(message);
+
+    return (
+        <Popup title={title} isOpen={isOpen} onClose={combFunc} >
+            <section className="info-tooltip">
+                <img className="info-tooltip__icon" src={iconLink} alt='Иконка типа сообщения' />
+                <p className="info-tooltip__message">{message}</p>
+            </section>
+        </Popup>
+    );
+}
+
+export default InfoTooltip;
