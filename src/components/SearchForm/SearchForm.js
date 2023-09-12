3a3720db5e0553de15bbc2f64d01185bf9c08524
losbojos <losbojos@yandex.ru>
2023-09-12 14:54:22 +0700

    Вывод ошибки к строке поиска - Нужно ввести ключевое слово

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react';
 import useFormAndValidation from '../../hooks/useFormAndValidation';
+import ErrorSpan from '../ErrorSpan/ErrorSpan';
+import { ERRORS } from '../../utils/consts';
 
 import './search-form.css';
 import './search-form__input.css';
 import './search-form__submit.css';
+import './search-form__input-container.css';
+import './search-form__error.css';
 
 function SearchForm({ searchPlaceholder, buttonSubmitText = 'Поиск', handleSearch, initialSearchString, onSearchStringChanged = null }) {
 
@@ -26,17 +30,37 @@ function SearchForm({ searchPlaceholder, buttonSubmitText = 'Поиск', handle
         handleSearch({ searchString: values[searchStringInput] });
     }
 
+    const preprocessHandleChange = (e) => {
+
+        const value = e.target.value;
+        const pattern = e.target.pattern;
+
+        if (!value.match(pattern)) {
+            e.target.setCustomValidity(ERRORS.EMPTY_SEARCH_STRING_ERROR);
+        } else {
+            e.target.setCustomValidity("");
+        }
+
+        handleChange(e);
+    }
+
     return (
         <form className='search-form' onSubmit={handleSubmit} noValidate>
-            <input
-                className='search-form__input'
-                type="text"
-                required
-                placeholder={searchPlaceholder}
-                name={searchStringInput}
-                value={values[searchStringInput] || ''}
-                onChange={handleChange}
-            />
+            <div className='search-form__input-container'>
+                <input
+                    id={searchStringInput}
+                    className='search-form__input'
+                    type="text"
+                    required
+                    placeholder={searchPlaceholder}
+                    name={searchStringInput}
+                    value={values[searchStringInput] || ''}
+                    onChange={preprocessHandleChange}
+                    pattern='.*\S.*' /* Должен быть хотя бы 1 непустой символ */
+                />
+                <ErrorSpan errors={errors[searchStringInput]} addStyles='search-form__error' />
+            </div>
+
             <button
                 className="search-form__submit"
                 disabled={!isValid}
